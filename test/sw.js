81e0b47d93146016163690df1788c9606fe5377e
tondam77-faith <tondam77@gmail.com>
2026-01-16 22:34:33 +0100

    Add files via upload

---
+++ test/sw.js
@@ -0,0 +1,3 @@
+const CACHE_NAME = 'history-test-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./index.html', './manifest.json', './icon-192.png']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
