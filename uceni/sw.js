8002e5e749f16e03ebdf5d73b46b50cc8fe1ee92
tondam77-faith <tondam77@gmail.com>
2026-01-16 23:33:01 +0100

    Verze 4

--- uceni/sw.js
+++ uceni/sw.js
@@ -1,39 +1,5 @@
-const CACHE_NAME = 'history-learn-v3'; // Verze cache
-
-// Seznam souborů, které se mají uložit pro offline použití
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './icon-learn.png'
-];
-
-// Instalace a uložení do cache
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// Aktivace a vyčištění staré cache (aby se načetla nová verze aplikace)
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
-
-// Získávání dat (když je offline, bere z cache)
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
-});
+const CACHE_NAME = 'history-learn-v4'; // Změna verze!
+const ASSETS = ['./index.html', './manifest.json', './icon-learn.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
