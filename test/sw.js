2ab10189b811d3d3f157fc00a37f0b91773ad8e9
tondam77-faith <tondam77@gmail.com>
2026-01-16 23:04:27 +0100

    Verze 2

--- test/sw.js
+++ test/sw.js
@@ -1,3 +1,39 @@
-const CACHE_NAME = 'history-test-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./index.html', './manifest.json', './icon-192.png']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'history-test-v2'; // Verze cache
+
+// Seznam souborů, které se mají uložit pro offline použití
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png'
+];
+
+// Instalace a uložení do cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivace a vyčištění staré cache
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Získávání dat
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
