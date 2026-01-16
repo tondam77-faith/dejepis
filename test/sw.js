7f5ce9b6a25ee7d79f67b91d2976f28f9df8533e
tondam77-faith <tondam77@gmail.com>
2026-01-16 23:32:29 +0100

    Verze 3

--- test/sw.js
+++ test/sw.js
@@ -1,39 +1,5 @@
-const CACHE_NAME = 'history-test-v2'; // Verze cache
-
-// Seznam souborů, které se mají uložit pro offline použití
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png'
-];
-
-// Instalace a uložení do cache
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// Aktivace a vyčištění staré cache
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
-
-// Získávání dat
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
-});
+const CACHE_NAME = 'history-test-v3'; // Změna verze!
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
